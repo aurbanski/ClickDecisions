9ce8a9eb1c56b0a33392457d7db08a37cb9d3f7d
aurbanski <aurbanski27@gmail.com>
2018-02-24 15:26:30 -0700

    Adding the database and other stuff

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 var express = require('express')
 var app = express()
+var bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const config = require('./config')
 
+var connection = "mongodb://" + config.username + ":" + config.password + "@ds147518.mlab.com:47518/clickdecisions"
+mongoose.connect(connection)
+var db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function(){
+  console.log("DB connected")
+})
+
+app.use(bodyParser.urlencoded({extended: true}))
 app.set('views', './views')
 app.set('view engine', 'pug')
 
 app.get('/', function(req, res){
   res.render('layout')
 });
+
+app.post('/user', function(req, res){
+  console.log(req.body)
+})
 
 app.listen(3000);
